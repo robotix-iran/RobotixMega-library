a88f85739c7c5e4a15f2867e5eab0954d6cfcb79
mahdi.h <mahdi2001h@gmail.com>
2025-02-02 11:01:16 +0330

    rewrite some examples

--- examples/FullTest/FullTest.ino
+++ examples/FullTest/FullTest.ino
@@ -2,22 +2,34 @@
 // Test RJ Modules (LED,SRF,IR)
 
 // Connection:
-// Port 1: LED
+// Port 1: SRF
 // Port 2: IR
-// Port 3: SRF
+// Port 3: LED
 //////////////////////////////////////////////////////////////////
 
 #include <RobotixMega.h>
+#include <Dynamixel2Arduino.h>
+
+
+
+#define DXL_SERIAL Serial3
+#define DEBUG_SERIAL Serial
+#define DXL_DIR_PIN 53
+const uint8_t DXL_ID = 5;
+const float DXL_PROTOCOL_VERSION = 1.0;
+Dynamixel2Arduino dxl(DXL_SERIAL, DXL_DIR_PIN);
+
 
 RobotixMega mega;
-
 #define PORT1 mega.port_1
 #define PORT2 mega.port_2
 #define PORT3 mega.port_3
 
+
+
 unsigned long startMillis;
 unsigned long currentMillis;
-const unsigned long period = 1000;
+const unsigned long period = 500;
 
 char temp[20];
 
@@ -25,63 +37,26 @@ char temp[20];
 
 void setup() {
   mega.begin();
-  mega.showLogo(LOGO_ROBOTIX);
   delay(1000);
   mega.clearLCD();
   mega.drawString(25, 10, "Playing:");
   mega.drawString(35, 30, "NOKIA");
-
   mega.playSong(NOKIA);
-  mega.rjSrfInit(PORT1);
+  mega.clearLCD();
+
   mega.rjIrInit(PORT2);
   mega.rjLedInit(PORT3);
 
   Serial.begin(115200);
-  Serial4.begin(115200);
-  pinMode(61, OUTPUT);
 
-
+  ////Motors //////////////////////////////////////////////////////////////
   mega.setMotorSpeed(mega.motor_1, 100);
   mega.setMotorSpeed(mega.motor_2, 100);
   mega.setMotorSpeed(mega.motor_3, 100);
   mega.setMotorSpeed(mega.motor_4, 100);
+  /////////////////////////////////////////////////////////////////////////
 
-
-
-
-  for (int i = 0; i < 10; i++) {
-    digitalWrite(61, HIGH);
-    delay(1);
-    Serial4.write(0xFF);
-    Serial4.write(0xFF);
-    Serial4.write(0x02);
-    Serial4.write(0x04);
-    Serial4.write(0x03);
-    Serial4.write(0x19);
-    Serial4.write((byte)0x00);
-    Serial4.write(0xDD);
-    delay(1);
-    digitalWrite(61, LOW);
-    delay(100);
-
-
-    digitalWrite(61, HIGH);
-    delay(1);
-    Serial4.write(0xFF);
-    Serial4.write(0xFF);
-    Serial4.write(0x02);
-    Serial4.write(0x04);
-    Serial4.write(0x03);
-    Serial4.write(0x19);
-    Serial4.write((byte)0x01);
-    Serial4.write(0xDC);
-    delay(1);
-    digitalWrite(61, LOW);
-    delay(100);
-  }
-
-
-
+  ////Servos //////////////////////////////////////////////////////////////
   mega.setServoPositions(mega.servo_1, 180);
   mega.setServoPositions(mega.servo_2, 180);
   mega.setServoPositions(mega.servo_3, 180);
@@ -90,20 +65,43 @@ void setup() {
   mega.setServoPositions(mega.servo_6, 180);
   mega.setServoPositions(mega.servo_7, 180);
   mega.setServoPositions(mega.servo_8, 180);
+  /////////////////////////////////////////////////////////////////////////
+
+  ////Dynamixel ///////////////////////////////////////////////////////////
+  dxl.begin(9600);
+  dxl.setPortProtocolVersion(DXL_PROTOCOL_VERSION);
+  dxl.ping(DXL_ID);
+
+  dxl.torqueOff(DXL_ID);
+  dxl.setOperatingMode(DXL_ID, OP_VELOCITY);
+  dxl.torqueOn(DXL_ID);
+  if (dxl.setGoalVelocity(DXL_ID, 128)) {
+    delay(1000);
+    DEBUG_SERIAL.print("Present Velocity : ");
+    DEBUG_SERIAL.println(dxl.getPresentVelocity(DXL_ID));
+    DEBUG_SERIAL.println();
+  }
+  /////////////////////////////////////////////////////////////////////////
 }
 
 // the loop function runs over and over again forever
 void loop() {
-
-  mega.rjLedWrite(PORT3, mega.rjIrRead(PORT2));
+  ////IMU      ///////////////////////////////////////////////////////////
+  float accx, accy, accz;
+  float gyrx, gyry, gyrz;
+  mega.getIMU(accx, accy, accz, gyrx, gyry, gyrz);
 
   currentMillis = millis();
   if (currentMillis - startMillis >= period) {
-    float distance = mega.rjSrfRead(PORT1);
-    sprintf(temp, "distance: %3.1f ", distance);
-    mega.clearLCD();
-    mega.drawString(0, 10, temp);
-    Serial.print(temp);
+    Serial.print("Gx:");
+    Serial.print(gyrx);
+    Serial.print(" Gy:");
+    Serial.print(gyry);
+    Serial.print(" Gz:");
+    Serial.println(gyrz);
     startMillis = currentMillis;
   }
+  /////////////////////////////////////////////////////////////////////////
+  
+  mega.rjLedWrite(PORT3, mega.rjIrRead(PORT2));
 }
--- examples/IMU/IMU.ino
+++ examples/IMU/IMU.ino
@@ -1,53 +1,45 @@
 #include <RobotixMega.h>
-#include <MadgwickAHRS.h>
-
 
 RobotixMega mega;
-Madgwick filter;
-
-unsigned long microsPerReading, microsPrevious;
 
 void setup() {
   mega.begin();
   Serial.begin(115200);
-  Serial.println("start");
-
-  filter.begin(25);
-
-  microsPerReading = 1000000 / 25;
-  microsPrevious = micros();
 }
 
-// the loop function runs over and over again forever
 void loop() {
   float accx, accy, accz;
   float gyrx, gyry, gyrz;
+  float magx, magy, magz;
 
-  float roll, pitch, heading;
-  unsigned long microsNow;
+  mega.getAccelerations(accx, accy, accz);
+  mega.getGyros(gyrx, gyry, gyrz);
+  mega.getMagno(magx, magy, magz);
 
-  mega.getIMU(accx, accy, accz, gyrx, gyry, gyrz);
+  Serial.print("\t\tAccel X: ");
+  Serial.print(accx);
+  Serial.print(" \tY: ");
+  Serial.print(accy);
+  Serial.print(" \tZ: ");
+  Serial.print(accz);
+  Serial.println(" m/s^2 ");
 
+  Serial.print("\t\tMag X: ");
+  Serial.print(magx);
+  Serial.print(" \tY: ");
+  Serial.print(magy);
+  Serial.print(" \tZ: ");
+  Serial.print(magz);
+  Serial.println(" uT");
 
-  microsNow = micros();
-  if (microsNow - microsPrevious >= microsPerReading) {
-    // update the filter, which computes orientation
-    filter.updateIMU(gyrx, gyry, gyrz, accx, accy, accz);
+  Serial.print("\t\tGyro X: ");
+  Serial.print(gyrx);
+  Serial.print(" \tY: ");
+  Serial.print(gyry);
+  Serial.print(" \tZ: ");
+  Serial.print(gyrz);
+  Serial.println(" radians/s ");
+  Serial.println();
 
-    // print the heading, pitch and roll
-    roll = filter.getRoll();
-    pitch = filter.getPitch();
-    heading = filter.getYaw();
-    Serial.print("Orientation: ");
-    Serial.print(heading);
-    Serial.print(" ");
-    Serial.print(pitch);
-    Serial.print(" ");
-    Serial.println(roll);
-
-
-    // increment previous time, so we keep proper pace
-    microsPrevious = microsPrevious + microsPerReading;
-  }
-
+  delay(100);
 }
